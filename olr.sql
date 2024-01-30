031f457cc0d11890a9319f5ddbd8f63918cef25a
5kydriven <tagpuno619@gmail.com>
2024-01-30 20:29:49 +0800

    updated sql

--- olr.sql
+++ olr.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 12, 2024 at 01:16 AM
+-- Generation Time: Jan 30, 2024 at 01:27 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,6 +20,46 @@ SET time_zone = "+00:00";
 --
 -- Database: `olr`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `email`, `password`) VALUES
+(1, 'admin@gmail.com', '0192023a7bbd73250516f069df18b500');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lesson`
+--
+
+CREATE TABLE `lesson` (
+  `id` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `title` varchar(60) NOT NULL,
+  `content` varchar(90) NOT NULL,
+  `link` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `lesson`
+--
+
+INSERT INTO `lesson` (`id`, `image`, `title`, `content`, `link`) VALUES
+(2, 'johny.jfif', 'The tips of sins', 'Hello there I&#39;m Johnny Sins, if you want to learn something in life click the button b', 'https://en.wikipedia.org/wiki/Johnny_Sins');
 
 -- --------------------------------------------------------
 
@@ -41,11 +81,24 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `name`, `email`, `password`, `gender`) VALUES
 (1, 'Wilson Laguda', 'wilson@gmail.com', '96e2273472b0f008b5a3dce47a4174da', 'male'),
-(4, 'Vesper Idyl Vencing', 'idyl@gmail.com', 'c36d1136c70796eb632906e27437e102', 'male');
+(4, 'Vesper Idyl Vencing', 'idyl@gmail.com', 'c36d1136c70796eb632906e27437e102', 'male'),
+(5, 'Mhel Angelo', 'tagpuno619@gmail.com', '202cb962ac59075b964b07152d234b70', 'male');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lesson`
+--
+ALTER TABLE `lesson`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -58,10 +111,22 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `lesson`
+--
+ALTER TABLE `lesson`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
